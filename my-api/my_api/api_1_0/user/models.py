698ce4e9c334d4c261d6e6cf0b4482ac307cec8f
Jorge Escobar <j@jorge3.com>
2023-04-13 12:01:28 -0400

    crud completed

--- my-api/my_api/api_1_0/user/models.py
+++ my-api/my_api/api_1_0/user/models.py
@@ -10,6 +10,8 @@ class UserModel(db.Model):
     User model
     """
 
+    __tablename__ = "user"
+
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String, unique=True, nullable=False)
     email = db.Column(db.String)
--- my-api/my_api/api_1_0/user/resources.py
+++ my-api/my_api/api_1_0/user/resources.py
@@ -14,8 +14,8 @@ user_ns = Namespace("user", description="User API")
 user_schema = UserSchema()
 user_list_schema = UserSchema(many=True)
 
-# Model for user post
-user_model = user_ns.model(
+# Model for user post/put requests
+user_req_model = user_ns.model(
     "User",
     {
         "username": fields.String(required=True),
@@ -29,10 +29,36 @@ class User(Resource):
 
     def get(self, id):
         """Get user by id"""
-        user_data = UserModel.find_by_id(id)
+        user_data = UserModel.query.filter_by(id=id).first()
         if user_data:
             return user_schema.dump(user_data)
         return {"message": "User not found"}, 404
+
+    def delete(self, id):
+        """Delete user by id"""
+        user_data = UserModel.query.filter_by(id=id)
+        if user_data:
+            user_data.delete()
+            db.session.commit()
+            return {"message": "User deleted successfully"}, 200
+        return {"message": "User not found"}, 404
+
+    @user_ns.expect(user_req_model)
+    def put(self, id):
+        """Update user by id"""
+        user_data = UserModel.query.filter_by(id=id).first()
+        user_json = request.get_json()
+
+        if not user_data:
+            return {"message": "User not found"}, 404
+
+        # update user
+        user_update_data = user_schema.load(user_json, session=db.session)
+        user_data.username = user_update_data.username
+        user_data.email = user_update_data.email
+
+        db.session.commit()
+        return user_schema.dump(user_data), 200
 
 
 class UserList(Resource):
@@ -40,11 +66,18 @@ class UserList(Resource):
     Endpoints for User API
     """
 
-    @user_ns.expect(user_model)
+    @user_ns.doc("Get all the Users")
+    def get(self):
+        """Get all users"""
+        return user_list_schema.dump(UserModel.query.all()), 200
+
+    @user_ns.expect(user_req_model)
+    @user_ns.doc("Create a User")
     def post(self):
         """Create new user"""
         user_json = request.get_json()
         user_data = user_schema.load(user_json, session=db.session)
-        user_data.save_to_db()
+        db.session.add(user_data)
+        db.session.commit()
 
         return user_schema.dump(user_data), 201
